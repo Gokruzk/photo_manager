430bc60dc71ec12d6924195678e7cc2b8187054f
Gokruzk <nigell@gmail.com>
2024-06-05 09:05:12 -0500

    feat: :sparkles: register user

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import {
   QueryClient,
   QueryClientProvider,
+  useMutation,
   useQuery,
 } from "@tanstack/react-query";
-import { Country } from "@/types";
+import { Country, User } from "@/types";
+import { addUser } from "@/api/userAPI";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +21,33 @@ export default function RegisterF() {
 }
 
 const RegisterForm = () => {
+  const registerUser = async (formdata: FormData) => {
+    const username = formdata.get("username") as string;
+    const email = formdata.get("email") as string;
+    const password = formdata.get("password") as string;
+    const birth = formdata.get("birth") as string;
+    const ubi = formdata.get("countries") as string;
+    const ub = Number(ubi)
+    const user = {
+      cod_ubi: ub,
+      cod_state: 1,
+      username: username,
+      email: email,
+      password: password,
+      birth_date: birth,
+    };
+
+    addUserMutation.mutate({
+      ...user,
+    });
+  };
+  const addUserMutation = useMutation({
+    mutationFn: addUser,
+    onSuccess: () => {
+      alert("User registered");
+    },
+  });
+
   const {
     isLoading,
     data: countries,
@@ -43,14 +72,14 @@ const RegisterForm = () => {
               <Link
                 href={"/"}
                 className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  {"<-"} Home
+              >
+                {"<-"} Home
               </Link>
             </p>
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Register an account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" action={registerUser}>
               <div>
                 <label
                   htmlFor="email"
@@ -68,19 +97,51 @@ const RegisterForm = () => {
                 />
               </div>
               <label
-                htmlFor="countries"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="birth"
+                className="block text-sm font-medium text-gray-900 dark:text-white"
               >
-                Select an option
+                Select your birth date
+              </label>
+              <div className="relative max-w-sm">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </div>
+                <input
+                  id="birth"
+                  name="birth"
+                  type="date"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Select date"
+                />
+              </div>
+
+              <label
+                htmlFor="countries"
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Select your country
               </label>
               <select
                 id="countries"
+                name="countries"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option selected>Choose a country</option>
+                <option defaultValue={"Choose a country"}>
+                  Choose a country
+                </option>
                 {countries.map((country: Country) => {
                   return (
-                    <option value={country.cod_ubi}>{country.country}</option>
+                    <option key={country.cod_ubi} value={country.cod_ubi}>
+                      {country.country}
+                    </option>
                   );
                 })}
               </select>
