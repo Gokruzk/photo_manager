e0ec3795849c14908b2f4f173e83f2dede0e193e
Gokruzk <nigell@gmail.com>
2024-06-25 15:21:19 -0500

    feat: :sparkles: edit form component

---
+++ components/ProfileEdit.tsx
@@ -0,0 +1,247 @@
+"use client";
+import { getCountries } from "@/api/countryAPI";
+import Link from "next/link";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
+import { Country, User } from "@/types";
+
+const queryClient = new QueryClient();
+
+interface EditProps {
+  user: User;
+}
+
+export default function Edit({ user }: EditProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProfileEdit user={user} />
+    </QueryClientProvider>
+  );
+}
+
+function ProfileEdit({ user }: EditProps) {
+  const registerUser = async (formdata: FormData) => {
+    const username = formdata.get("username") as string;
+    const email = formdata.get("email") as string;
+    const password = formdata.get("password") as string;
+    const birth = formdata.get("birth") as string;
+    const ubi = formdata.get("countries") as string;
+    const ub = Number(ubi);
+    const user = {
+      cod_ubi: ub,
+      cod_state: 1,
+      username: username,
+      email: email,
+      password: password,
+      birth_date: birth,
+    };
+  };
+
+  const {
+    isLoading,
+    data: countries,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["countries"],
+    queryFn: getCountries,
+    retry: 3,
+    retryDelay: (attempt) => Math.min(1000 * 2 ** attempt, 30000),
+    refetchInterval: 2000, // Obtención en tiempo real cada 2 segundos
+  });
+
+  function togglePasswordVisibility() {
+    const passwordInput = document.getElementById(
+      "password"
+    ) as HTMLInputElement | null;
+
+    if (passwordInput) {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+      } else {
+        passwordInput.type = "password";
+      }
+    }
+  }
+
+  if (isLoading) return <div>Loading...</div>;
+  else if (isError) return <div>Error {error.message}</div>;
+
+  return (
+    <main className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <Link
+                href={"/profile"}
+                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                {"<-"} Back
+              </Link>
+            </p>
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Register an account
+            </h1>
+            <form className="space-y-4 md:space-y-6" action={registerUser}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="email@email.com"
+                  value={user.email}
+                  required
+                />
+                <label
+                  htmlFor="username"
+                  className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your username
+                </label>
+                <input
+                  type="username"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="username"
+                  value={user.username}
+                  required
+                />
+              </div>
+
+              <label
+                htmlFor="birth"
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Select your birth date
+              </label>
+              <div className="relative max-w-sm">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </div>
+                <input
+                  id="birth"
+                  name="birth"
+                  type="date"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Select date"
+                  value={user.birth_date}
+                />
+              </div>
+
+              <label
+                htmlFor="countries"
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Select your country
+              </label>
+              <select
+                id="countries"
+                name="countries"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option defaultValue={"Choose a country"}>
+                  Choose a country
+                </option>
+                {countries.map((country: Country) => {
+                  return (
+                    <option key={country.cod_ubi} value={country.cod_ubi}>
+                      {country.country}
+                    </option>
+                  );
+                })}
+              </select>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your new password
+                </label>
+                <div className="relative">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter password"
+                    required
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 px-3 py-2"
+                    onClick={togglePasswordVisibility}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 text-gray-400 dark:text-gray-300"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M9.88 9.88a3 3 0 1 0 4.24 4.24"
+                      ></path>
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
+                      ></path>
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
+                      ></path>
+                      <line
+                        className="hs-password-active:hidden"
+                        x1="2"
+                        x2="22"
+                        y1="2"
+                        y2="22"
+                      ></line>
+                      <path
+                        className="hidden hs-password-active:block"
+                        d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"
+                      ></path>
+                      <circle
+                        className="hidden hs-password-active:block"
+                        cx="12"
+                        cy="12"
+                        r="3"
+                      ></circle>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Update
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
