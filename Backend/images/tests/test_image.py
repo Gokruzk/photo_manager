dc38931d6b39ad06223e8d998a203c4789718147
gokruzk <nigelljama@gmail.com>
2025-09-08 17:12:38 -0500

    refactor: :white_check_mark: improve tests structure with POO

--- Backend/images/tests/test_image.py
+++ Backend/images/tests/test_image.py
@@ -8,9 +8,15 @@ from images.domain.value_objects import DateCod, ImageCod, UbicationCod, UserCod
 from images.domain.exceptions import ImageDeletedError, ImageNotFoundError, ImageUploadError
 
 
+@pytest.mark.usefixtures("mock_image_repository")
 class TestImageService:
+    @pytest.fixture(autouse=True)
+    def setup_service(self, mock_image_repository):
+        self.mock_image_repository = mock_image_repository
+        self.service = ImageService(mock_image_repository)
+
     @pytest.mark.asyncio
-    async def test_get_by_cod_success(self, mock_image_repository):
+    async def test_get_by_cod_success(self):
         # Arrange
         cod_image = uuid4()
         cod_user = uuid4()
@@ -23,34 +29,31 @@ class TestImageService:
             uploadedat=DateCod(value=20010101)
         )
 
-        mock_image_repository.find_by_cod = AsyncMock(
+        self.mock_image_repository.find_by_cod = AsyncMock(
             return_value=expected_image)
 
-        service = ImageService(mock_image_repository)
-
         # Act
-        result = await service.find_by_cod(cod_image)
+        result = await self.service.find_by_cod(cod_image)
 
         # Assert
         assert result == expected_image
-        mock_image_repository.find_by_cod.assert_called_once_with(cod_image)
+        self.mock_image_repository.find_by_cod.assert_called_once_with(
+            cod_image)
 
     @pytest.mark.asyncio
-    async def test_get_not_found(self, mock_image_repository):
+    async def test_get_not_found(self):
         # Arrange
         cod_image = uuid4()
 
-        mock_image_repository.find_by_cod = AsyncMock(
+        self.mock_image_repository.find_by_cod = AsyncMock(
             return_value=None)
-
-        service = ImageService(mock_image_repository)
 
         # Act& Assert
         with pytest.raises(ImageNotFoundError, match=f"The image with id {cod_image} not found"):
-            await service.find_by_cod(cod_image)
+            await self.service.find_by_cod(cod_image)
 
     @pytest.mark.asyncio
-    async def test_upload_success(self, fake_upload_file, mock_image_repository):
+    async def test_upload_success(self, fake_upload_file):
         # Arrange
         cod_image = uuid4()
         cod_user = uuid4()
@@ -63,38 +66,36 @@ class TestImageService:
             uploadedat=DateCod(value=20010101)
         )
 
-        mock_image_repository.upload = AsyncMock(
+        self.mock_image_repository.upload = AsyncMock(
             return_value=expected_image)
 
-        service = ImageService(mock_image_repository)
         image = UploadImage(cod_user=UserCod(value=cod_user),
                             cod_ubi=UbicationCod(value=1), image_path="/image/path")
 
         # Act
-        result = await service.upload(image, fake_upload_file)
+        result = await self.service.upload(image, fake_upload_file)
 
         # Assert
         assert result == expected_image
-        await service.upload(image, fake_upload_file)
+        await self.service.upload(image, fake_upload_file)
 
     @pytest.mark.asyncio
-    async def test_upload_failed(self, fake_upload_file, mock_image_repository):
+    async def test_upload_failed(self, fake_upload_file):
         # Arrange
         cod_user = uuid4()
 
-        mock_image_repository.upload = AsyncMock(
+        self.mock_image_repository.upload = AsyncMock(
             return_value=None)
 
-        service = ImageService(mock_image_repository)
         image = UploadImage(cod_user=UserCod(value=cod_user),
                             cod_ubi=UbicationCod(value=1), image_path="/image/path")
 
         # Act & Assert
         with pytest.raises(ImageUploadError, match=f"Unexpected error uploading image for user {cod_user}"):
-            await service.upload(image, fake_upload_file)
+            await self.service.upload(image, fake_upload_file)
 
     @pytest.mark.asyncio
-    async def test_delete_success(self, mock_image_repository):
+    async def test_delete_success(self):
         # Arrange
         cod_image = uuid4()
         cod_user = uuid4()
@@ -107,35 +108,32 @@ class TestImageService:
             uploadedat=DateCod(value=20010101)
         )
 
-        mock_image_repository.delete = AsyncMock(
+        self.mock_image_repository.delete = AsyncMock(
             return_value=expected_image)
 
-        service = ImageService(mock_image_repository)
-
         # Act
-        result = await service.delete(cod_image)
+        result = await self.service.delete(cod_image)
 
         # Assert
         assert result == expected_image
-        mock_image_repository.delete.assert_called_once_with(cod_image)
+        self.mock_image_repository.delete.assert_called_once_with(cod_image)
 
     @pytest.mark.asyncio
-    async def test_delete_not_found(self, mock_image_repository):
+    async def test_delete_not_found(self):
         # Arrange
         cod_image = uuid4()
 
-        mock_image_repository.delete = AsyncMock(return_value=None)
+        self.mock_image_repository.delete = AsyncMock(return_value=None)
 
-        service = ImageService(mock_image_repository)
-        service.find_by_cod = AsyncMock(side_effect=ImageNotFoundError(
+        self.service.find_by_cod = AsyncMock(side_effect=ImageNotFoundError(
             f"The image with id {cod_image} not found"))
 
         # Act& Assert
         with pytest.raises(ImageNotFoundError):
-            await service.delete(cod_image)
+            await self.service.delete(cod_image)
 
     @pytest.mark.asyncio
-    async def test_delete_failed(self, mock_image_repository):
+    async def test_delete_failed(self):
         # Arrange
         cod_image = uuid4()
         cod_user = uuid4()
@@ -148,12 +146,11 @@ class TestImageService:
             uploadedat=DateCod(value=20010101)
         )
 
-        mock_image_repository.delete = AsyncMock(
+        self.mock_image_repository.delete = AsyncMock(
             return_value=None)
 
-        service = ImageService(mock_image_repository)
-        service.find_by_cod = AsyncMock(return_value=check_image)
+        self.service.find_by_cod = AsyncMock(return_value=check_image)
 
         # Act & Assert
         with pytest.raises(ImageDeletedError):
-            await service.delete(cod_image)
+            await self.service.delete(cod_image)
