bf4b025f71d77751f348151a591952c8639437b3
gokruzk <nigelljama@gmail.com>
2025-09-11 21:23:42 -0500

    feat: :sparkles: singleton to db connection

--- src/auth/infra/database/postgres/prisma_connection.py
+++ src/auth/infra/database/postgres/prisma_connection.py
@@ -1,7 +1,17 @@
 from .generated.auth_client import Prisma as PrismaAuth
 
 
-class PrismaConnection():
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class PrismaManager(metaclass=SingletonMeta):
     def __init__(self):
         self.prisma = PrismaAuth()
 
