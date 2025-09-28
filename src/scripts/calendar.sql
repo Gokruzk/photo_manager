ef3dea8c4185fee672e86806831d1e6e27fca1b4
gokruzk <nigelljama@gmail.com>
2025-09-27 19:54:58 -0500

    feat: :seedling: calendar script for pg and mysql

--- src/scripts/calendar.sql
+++ src/scripts/calendar.sql
@@ -1,3 +1,4 @@
+-- POSTGRES
 CREATE OR REPLACE FUNCTION SP_CALENDARIO()
 RETURNS VOID AS $$
 		DECLARE
@@ -21,3 +22,29 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT sp_calendario()
+
+--MYSQL
+DELIMITER $$
+
+CREATE PROCEDURE SP_CALENDARIO()
+BEGIN
+    DECLARE Fecha DATE;
+    DECLARE FechaFinal DATE;
+
+    SET Fecha = '2000-01-01';
+    SET FechaFinal = '2100-12-31';
+
+    WHILE Fecha <= FechaFinal DO
+        INSERT INTO dates (cod_date, `day`)
+        VALUES (
+            DATE_FORMAT(Fecha, '%Y%m%d'),
+            Fecha
+        );
+        SET Fecha = DATE_ADD(Fecha, INTERVAL 1 DAY);
+    END WHILE;
+END$$
+
+DELIMITER ;
+
+-- Ejecutar el procedimiento
+CALL SP_CALENDARIO();
